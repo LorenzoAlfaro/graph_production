feab9bbe1cdff461f2648e153bd71867832cf8f1
Lorenzo Alfaro <lorenzoac88@gmail.com>
2022-02-21 13:24:58 -0700

    new stats WIP

---
+++ functions/stats.py
@@ -0,0 +1,164 @@
+import os
+from numpy import average
+import sc2reader
+import matplotlib.pyplot as plt
+
+from prettyPrinter import formatReplay
+
+
+path = 'C:/Users/loren/Documents/StarCraft II/Accounts/66185323/1-S2-2-818362/Replays/Multiplayer/'
+
+def worker_counter(replay, sec, player_id):
+
+    frame = sec * 22.404
+    workers = []
+    for event in replay.events:
+        if event.name == "UnitBornEvent" and event.control_pid == player_id:
+            if event.unit.is_worker:
+                workers.append(event.unit)
+    
+        if event.name == "UnitDiedEvent":
+            if event.unit in workers:
+                workers.remove(event.unit)
+        
+        if event.frame > frame:
+            break
+    
+    return len(workers)
+
+def getMyTeamnumber(replay, name):        
+    for key in replay.player:
+        
+        if replay.player[key].name == name:
+            return replay.player[key].pid
+            
+def printName(replay):
+
+    if replay.is_ladder:
+        
+        print(replay.player[1].name)
+        print(replay.player[1].pick_race)
+        print(replay.player[1].pid)
+        print(replay.player[1].result)
+    
+
+
+def avgWorkerAtSec(file, sec):
+    path = 'C:/Users/loren/Documents/StarCraft II/Accounts/66185323/1-S2-2-818362/Replays/Multiplayer/'
+    files = os.listdir(path)
+    avg = []
+    avg2 = []
+    
+    for f in files:
+        try:
+            replay = sc2reader.load_replay(path + f, load_map=False)
+
+            if replay.is_ladder and replay.type == '1v1' and replay.frames > (sec * 22.404):                                        
+                pid = getMyTeamnumber(replay,'VanKiwi')
+                if pid ==1:
+                    pid2 = 2
+                else:
+                    pid2 = 1
+
+                print(formatReplay(replay))
+                wc = worker_counter(replay, 360, pid)
+                avg.append(wc)
+                wc2 = worker_counter(replay, 360, pid2)
+                avg2.append(wc2)
+
+                print("player 1:",wc)
+                print("player 2:",wc2)
+        except Exception as e:
+            print(e)
+
+    print('promedio')
+    print( average(avg))
+    print( average(avg2))
+
+
+def logWorker(replay, file, sec):    
+    length_of_game = replay.frames // 22.404
+
+    
+
+    # 22.404 frames per second
+    file.write(formatReplay(replay))
+    file.write("player 1: " + str(worker_counter(replay, sec, 1)) + '\n')
+    file.write("player 2: " + str(worker_counter(replay, sec, 2)) + '\n')
+    file.write(str(length_of_game/60) + '\n')
+    file.write(str(replay.frames) + '\n')
+
+def graphWorker(replay):
+    length_of_game = replay.frames // 22.404
+    workers_1 = [worker_counter(replay, k, 1) for k in range(int(length_of_game)+1)]
+    workers_2 = [worker_counter(replay, k, 2) for k in range(int(length_of_game)+1)]
+
+    plt.figure()
+    plt.plot(workers_1, label=replay.players[0])
+    plt.plot(workers_2, label=replay.players[1])
+    plt.legend(loc=2)
+    plt.show()
+
+
+# unit_born_events = events_of_type["UnitBornEvent"]
+
+# for ube in unit_born_events:
+
+#     if ube.unit.name != "InvisibleTargetDummy":
+        
+#         print("{} created {} at second {}".format(ube.unit_controller,
+#             ube.unit,
+#             ube.second))
+
+# unit_init_events = events_of_type["UnitInitEvent"]
+
+# for uie in unit_init_events:
+#     print("{} started creating {} at second {}".format(uie.unit_controller,
+#                                                        uie.unit,
+#                                                        uie.second))
+
+# unit_done_events = events_of_type["UnitDoneEvent"]
+
+# for ude in unit_done_events:
+#     print("{} finished".format(ude.unit))
+
+# unit_died_events = events_of_type["UnitDiedEvent"]
+
+# for udiede in unit_died_events:
+#     print("{} was killed by {} using {} at ({}, {})".format(udiede.unit,
+#                                                             udiede.killer,
+#                                                             udiede.killing_unit,
+#                                                             udiede.x,
+#                                                             udiede.y))
+
+
+
+
+# python .\prettyPrinter.py "C:\Users\loren\Documents\StarCraft II\Accounts\66185323\1-S2-2-818362\Replays\Multiplayer\Blackburn LE (47).SC2Replay"
+# https://www.miguelgondu.com/blogposts/2018-09-03/a-tutorial-on-sc2reader-events-and-units/
+
+# TODO 6 minutes metrics
+# how many workers did I loose on the first 6 minutes
+# how long does it take to the get 3thre base saturation?
+# how long to max out? 8:00 max roach metric
+# what types of units kill my drones? adepts? reaper? liberator? hellions?
+# worker graphs
+# creep tumor count
+# overlord spread
+# overseer changeling
+
+# basic build opener of my oponent
+
+# analize builds I loose to the most
+# analize map records
+
+# control group management - hard to analize
+# analize my scouts - hard
+# check location of enemy tech on the position of my units and vision? hard
+# camera movements? how long does it take to respond to an attack or harrasment
+# detect misclicks? hard
+
+# make it run in the background
+# save data in a database
+# sort my replays and rename them 
+
--- parser.py
+++ parser.py
@@ -1,76 +1,44 @@
+from functions.stats import avgWorkerAtSec, worker_counter, graphWorker, logWorker
+from prettyPrinter import formatReplay
 import sc2reader
 import matplotlib.pyplot as plt
 import os
 
 path = 'C:/Users/loren/Documents/StarCraft II/Accounts/66185323/1-S2-2-818362/Replays/Multiplayer/'
+file = 'Pride of Altaris LE (6).SC2Replay'
 
-def worker_counter(replay, second, player_id):
-    workers = []
-    for event in replay.events:
-        if event.name == "UnitBornEvent" and event.control_pid == player_id:
-            if event.unit.is_worker:
-                workers.append(event.unit)
-    
-        if event.name == "UnitDiedEvent":
-            if event.unit in workers:
-                workers.remove(event.unit)
-        
-        if event.second > second:
-            break
-    
-    return len(workers)
-
-
-files = os.listdir(path)
-
-for f in files:
-	print(f)
-
-path = 'C:\\Users\\loren\\Documents\\StarCraft II\\Accounts\\66185323\\1-S2-2-818362\\Replays\\Multiplayer\\'
-file = 'Blackburn LE (47).SC2Replay'
+f = open("workerslost.txt", "a")
+f.write("Now the file has more content!")
 replay = sc2reader.load_replay(path + file, load_map=False)
 
-event_names = set([event.name for event in replay.events])
+avgWorkerAtSec(f,360)
+# logWorker(replay,f, 360)
+# graphWorker(replay)
 
-events_of_type = {name: [] for name in event_names}
-for event in replay.events:
-    events_of_type[event.name].append(event)
+f.close()
 
-unit_born_events = events_of_type["UnitBornEvent"]
+# replay = sc2reader.load_replay(path + file, load_map=False)
 
-# for ube in unit_born_events:
+# create dictionary of events and their types
+# event_names = set([event.name for event in replay.events])
+# events_of_type = {name: [] for name in event_names}
+# for event in replay.events:
+#     events_of_type[event.name].append(event)
 
-#     if ube.unit.name != "InvisibleTargetDummy":
-        
-#         print("{} created {} at second {}".format(ube.unit_controller,
-#             ube.unit,
-#             ube.second))
+# unit_born_events = events_of_type["UnitBornEvent"]
 
-unit_init_events = events_of_type["UnitInitEvent"]
+# unit_init_events = events_of_type["UnitInitEvent"]
 
-# for uie in unit_init_events:
-#     print("{} started creating {} at second {}".format(uie.unit_controller,
-#                                                        uie.unit,
-#                                                        uie.second))
+# unit_done_events = events_of_type["UnitDoneEvent"]
 
-unit_done_events = events_of_type["UnitDoneEvent"]
+# unit_died_events = events_of_type["UnitDiedEvent"]
 
-# for ude in unit_done_events:
-#     print("{} finished".format(ude.unit))
-
-unit_died_events = events_of_type["UnitDiedEvent"]
-
-# for udiede in unit_died_events:
-#     print("{} was killed by {} using {} at ({}, {})".format(udiede.unit,
-#                                                             udiede.killer,
-#                                                             udiede.killing_unit,
-#                                                             udiede.x,
-#                                                             udiede.y))
+# length_of_game = replay.frames // 24
 
 
-length_of_game = replay.frames // 24
-workers_1 = [worker_counter(replay, k, 1) for k in range(length_of_game)]
-workers_2 = [worker_counter(replay, k, 2) for k in range(length_of_game)]
+
+# workers_1 = [worker_counter(replay, k, 1) for k in range(length_of_game)]
+# workers_2 = [worker_counter(replay, k, 2) for k in range(length_of_game)]
 
 # plt.figure()
 # plt.plot(workers_1, label=replay.players[0])
