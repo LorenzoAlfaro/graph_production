e5a600b4f80ba22e87cb73251d50360c49763d1a
Lorenzo Alfaro <lorenzoac88@gmail.com>
2022-03-04 22:07:19 -0700

    Added delay before trying to parse file

--- ImagesEventHandler.py
+++ ImagesEventHandler.py
@@ -1,9 +1,11 @@
+from asyncio import TimerHandle
 import os
 from PIL import Image
 from PIL.ImageOps import grayscale
 from watchdog.events import RegexMatchingEventHandler
 from functions.stats import avgWorkerAtSec, getMyTeamnumber, worker_counter, graphWorker, logWorker, worker_counter2
 import sc2reader
+import time
 
 class ImagesEventHandler(RegexMatchingEventHandler):
     THUMBNAIL_SIZE = (128, 128)
@@ -16,6 +18,7 @@ class ImagesEventHandler(RegexMatchingEventHandler):
     def on_created(self, event):
         filename, ext = os.path.splitext(event.src_path)
         print('created ' + filename +"  " + ext)
+        time.sleep(1)
         self.newReplay(filename + ext)
         # self.process(event)
 
@@ -30,35 +33,41 @@ class ImagesEventHandler(RegexMatchingEventHandler):
 
 
     def newReplay(self, fileName):
-        try:
-            replay = sc2reader.load_replay(fileName, load_map=False)
+
+        if ".writeCacheBackup" in fileName:
+            "print not replay"
+            return
+        
+        replay = sc2reader.load_replay(fileName, load_map=False)
+        
+        # graphWorker(replay)
+
+
+        # if replay.is_ladder and replay.type == '1v1' and replay.frames > (sec * 22.404):                                        
+        # create dictionary of events and their types
+        length_of_game = replay.frames // 22.404
+
+        event_names = set([event.name for event in replay.events])
+        events_of_type = {name: [] for name in event_names}
+        for event in replay.events:
+            events_of_type[event.name].append(event)
+
+        pid = getMyTeamnumber(replay,'VanKiwi')
+        if pid ==1:
+            pid2 = 2
+        else:
+            pid2 = 1
             
-            # graphWorker(replay)
-
-
-            # if replay.is_ladder and replay.type == '1v1' and replay.frames > (sec * 22.404):                                        
-            # create dictionary of events and their types
-            length_of_game = replay.frames // 22.404
-
-            event_names = set([event.name for event in replay.events])
-            events_of_type = {name: [] for name in event_names}
-            for event in replay.events:
-                events_of_type[event.name].append(event)
-
-            pid = getMyTeamnumber(replay,'VanKiwi')
-            if pid ==1:
-                pid2 = 2
-            else:
-                pid2 = 1
-                
-            self.printWorkerCount(60,events_of_type,pid,pid2)
-            self.printWorkerCount(120,events_of_type,pid,pid2)
-            self.printWorkerCount(180,events_of_type,pid,pid2)
-            self.printWorkerCount(240,events_of_type,pid,pid2)
-            self.printWorkerCount(300,events_of_type,pid,pid2)
-            self.printWorkerCount(360,events_of_type,pid,pid2)
-            self.printWorkerCount(420,events_of_type,pid,pid2)            
+        self.printWorkerCount(60,events_of_type,pid,pid2)
+        self.printWorkerCount(120,events_of_type,pid,pid2)
+        self.printWorkerCount(180,events_of_type,pid,pid2)
+        self.printWorkerCount(240,events_of_type,pid,pid2)
+        self.printWorkerCount(300,events_of_type,pid,pid2)
+        self.printWorkerCount(360,events_of_type,pid,pid2)
+        self.printWorkerCount(420,events_of_type,pid,pid2)            
                 # logWorker(file, replay.filename, wc,wc2, replay.player, pid, pid2, length_of_game, replay.start_time)
+        try:
+            print('hel;')
                
         except Exception as e:
             print(e)
