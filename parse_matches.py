370725b91b1dde55f43b9992e5a20087f2c05b07
Lorenzo Alfaro <lorenzoac88@gmail.com>
2023-01-02 13:44:38 -0700

    feat: create method to update replay table

--- parse_matches.py
+++ parse_matches.py
@@ -13,7 +13,8 @@ def insert_player(db_connection, db_cursor, player):
         tag = bytes(player.clan_tag, 'utf-8')
         tag2 = bytes.decode(tag,'utf-8')
 
-    sql = f"INSERT INTO player_table (uid, player_name, clan_tag, region, subregion, player_url) VALUES ({uid}, '{player.name}', '{tag2}', '{player.region}', {player.subregion}, '{player.url}')"
+    sql = f"INSERT INTO player_table (uid, player_name, clan_tag, region, subregion, player_url) VALUES (\
+        {uid}, '{player.name}', '{tag2}', '{player.region}', {player.subregion}, '{player.url}')"
     db_cursor.execute(sql)
     db_connection.commit()
 
@@ -90,6 +91,21 @@ def update_match_record(db_connection, db_cursor, player, opponent):
         db_cursor.execute(sql_update)
         db_connection.commit()
 
+def register_replay(db_connection, db_cursor, player, opponent, replay):
+    opponent_uid = opponent.detail_data['bnet']['uid']
+    player_uid = player.detail_data['bnet']['uid']
+    sql = f"SELECT * FROM replay_table WHERE file_hash = '{replay.filehash}'"
+    db_cursor.execute(sql)
+    myresult = db_cursor.fetchall()
+
+    if len(myresult) == 0:
+        sql_insert = f"INSERT INTO replay_table\
+            (file_hash, player_updated, match_updated, path, player_1, player_2, race_1, race_2, result, map_name, frames)\
+            VALUES ('{replay.filehash}', 1, 1, '{replay.filename}', {player_uid}, {opponent_uid}, '{player.play_race}', '{opponent.play_race}',\
+            '{player.result}', '{replay.map_name}', {replay.frames})"
+        # print(sql_insert)
+        db_cursor.execute(sql_insert)
+        db_connection.commit()
 
 def update_db(mydb, mycursor, file_path):
     try:
@@ -127,5 +143,35 @@ def update_db(mydb, mycursor, file_path):
         # raise e
         print("-------------------------------")
         # print(traceback.format_exc())
+        print(e, file_path)
+        print("-------------------------------")
+
+def update_replay(mydb, mycursor, file_path:str):
+    try:
+        sec = 1
+        if ".writeCacheBackup" in file_path:
+            print("not a replay file")
+            return
+        replay = sc2.load_replay(file_path, load_map = False)
+        # print(f)
+        if replay.type != '1v1' or replay.frames < (sec * 22.404):
+            return
+
+        opponent = replay.players[0]
+        player = replay.players[1]
+        opp_uid = opponent.detail_data['bnet']['uid']
+
+        if opp_uid == 818362 or opp_uid == 1931022:
+            opponent = replay.players[1]
+            player = replay.players[0]
+
+        if not opponent.is_human :
+            return
+
+        register_replay(mydb, mycursor, player, opponent, replay)
+    except Exception as e:
+        # raise e
+        print("-------------------------------")
+        print(traceback.format_exc())
         print(e, file_path)
         print("-------------------------------")
