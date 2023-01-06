de95852df641ecd78739ff904f458e43528436fc
Lorenzo Alfaro <lorenzoac88@gmail.com>
2023-01-05 17:29:05 -0700

    feat: print plot of drones

--- parse_replays.py
+++ parse_replays.py
@@ -4,14 +4,17 @@ import sc2reader as sc2
 import traceback
 from decouple import config
 from sc2reader.objects import Player
+from functions.stats import analize_drone_kills, copy_replay, plot_drone_kills, worker_timeline
 
 from parse_matches import update_db, update_replay
-
+import shutil
+import os
 
 password = config('MYSQL_PASSWORD')
 user = config('USER_NAME')
 path = 'C:/Users/loren/Documents/StarCraft II/Accounts/66185323/1-S2-2-818362/Replays/Multiplayer/'
 path_2 = 'C:/Users/loren/Documents/StarCraft II/Accounts/66185323/1-S2-1-1931022/Replays/Multiplayer/'
+save_path = 'C:/Users/loren/Documents/StarCraft II/Accounts/66185323/1-S2-1-1931022/Replays/Multiplayer/Analisis/'
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -23,10 +26,25 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 # open folder of replays
 files = os.listdir(path)
-
 files_2 = os.listdir(path_2)
 
-sec = 1 # second you are interesed at
+get_replays_sql = f"SELECT path FROM trainer.replay_table where player_2 = 6556840 and \
+    (player_1 = 1931022 or player_1 = 818362) and race_1 = 'Zerg' and race_2 = 'Terran' and result = 'Loss';"
+
+# SELECT * FROM trainer.replay_table where file_hash like '%a4b37353e7';
+
+mycursor.execute(get_replays_sql)
+myresult = mycursor.fetchall()
+
+for f in myresult:
+    replay = sc2.load_replay(f[0], load_map = False)
+    worker_timeline(replay)
+    # break
+    # copy_replay(f[0], replay, save_path)
+    # analize_drone_kills(replay, 6556840)
+
+exit()
+
 
 for f in files:
     update_db(mydb, mycursor, path + f)
