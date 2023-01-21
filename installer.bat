c10dc8a830fc559808b003714df5d0d4480a7227
Lorenzo Alfaro <lorenzoac88@gmail.com>
2023-01-21 15:30:18 -0700

    fix: replace hard coded path with variable

--- installer.bat
+++ installer.bat
@@ -1,4 +1,4 @@
-IF NOT EXIST C:\Users\e420882\AppData\Local\Programs\my_app md C:\Users\e420882\AppData\Local\Programs\my_app
-powershell -command "Start-BitsTransfer -Source http://localhost:8000/targets/my_app-1.0.tar.gz -Destination C:\Users\e420882\AppData\Local\Programs\my_app\my_app-1.0.tar.gz"
-tar -xf C:\Users\e420882\AppData\Local\Programs\my_app\my_app-1.0.tar.gz -C C:\Users\e420882\AppData\Local\Programs\my_app\
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\my_app.lnk');$s.TargetPath='%userprofile%\AppData\Local\Programs\my_app\main.exe';$s.Arguments='connect';$s.WorkingDirectory='%userprofile%\AppData\Local\Programs\my_app';$s.Save()"
+IF NOT EXIST %userprofile%\AppData\Local\Programs\my_app md %userprofile%\AppData\Local\Programs\my_app
+powershell -command "Start-BitsTransfer -Source https://deploymentapps.z22.web.core.windows.net/ggtrainer/targets/my_app-1.0.tar.gz -Destination %userprofile%\AppData\Local\Programs\my_app\my_app-1.0.tar.gz"
+tar -xf %userprofile%\AppData\Local\Programs\my_app\my_app-1.0.tar.gz -C %userprofile%\AppData\Local\Programs\my_app\
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\my_app.lnk');$s.TargetPath='%userprofile%\AppData\Local\Programs\my_app\app.exe';$s.Arguments='connect';$s.WorkingDirectory='%userprofile%\AppData\Local\Programs\my_app';$s.Save()"
